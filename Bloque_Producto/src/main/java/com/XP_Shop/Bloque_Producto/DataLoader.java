1770d550f7897795251a7cd40742b741dfa5f0d7
unknown <clau.osoriot@duocuc.cl>
2026-06-22 21:38:54 -0400

    bloque producto datafaker terminado y comienzo gateway

--- Bloque_Producto/src/main/java/com/XP_Shop/Bloque_Producto/DataLoader.java
+++ Bloque_Producto/src/main/java/com/XP_Shop/Bloque_Producto/DataLoader.java
@@ -1,25 +1,27 @@
 package com.XP_Shop.Bloque_Producto;
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import com.XP_Shop.Bloque_Producto.controller.CategoriaController;
-import com.XP_Shop.Bloque_Producto.controller.CategoriasController;
-import com.XP_Shop.Bloque_Producto.controller.ImagenController;
-import com.XP_Shop.Bloque_Producto.controller.MarcaController;
-import com.XP_Shop.Bloque_Producto.controller.MarcasController;
-import com.XP_Shop.Bloque_Producto.controller.ProductoController;
-import com.XP_Shop.Bloque_Producto.controller.ProductosController;
 import com.XP_Shop.Bloque_Producto.model.Categoria;
 import com.XP_Shop.Bloque_Producto.model.Categorias;
 import com.XP_Shop.Bloque_Producto.model.Imagen;
 import com.XP_Shop.Bloque_Producto.model.Marca;
 import com.XP_Shop.Bloque_Producto.model.Marcas;
 import com.XP_Shop.Bloque_Producto.model.Producto;
+import com.XP_Shop.Bloque_Producto.model.Productos;
+import com.XP_Shop.Bloque_Producto.repository.CategoriaRepository;
+import com.XP_Shop.Bloque_Producto.repository.CategoriasRepository;
+import com.XP_Shop.Bloque_Producto.repository.ImagenRepository;
+import com.XP_Shop.Bloque_Producto.repository.MarcaRepository;
+import com.XP_Shop.Bloque_Producto.repository.MarcasRepository;
+import com.XP_Shop.Bloque_Producto.repository.ProductoRepository;
+import com.XP_Shop.Bloque_Producto.repository.ProductosRepository;
 
 import net.datafaker.Faker;
 
@@ -28,65 +30,120 @@ import net.datafaker.Faker;
 public class DataLoader implements CommandLineRunner{
 
     @Autowired
-    private CategoriaController categoriaController;
+    private CategoriaRepository categoriaRepository;
     @Autowired
-    private CategoriasController categoriasController;
+    private CategoriasRepository categoriasRepository;
     @Autowired
-    private ImagenController imagenController;
+    private ImagenRepository imagenRepository;
     @Autowired 
-    private MarcaController marcaController;
+    private MarcaRepository marcaRepository;
     @Autowired
-    private MarcasController marcasController;
+    private MarcasRepository marcasRepository;
     @Autowired
-    private ProductoController productoController;
+    private ProductoRepository productoRepository;
     @Autowired
-    private ProductosController productosController;
+    private ProductosRepository productosRepository;
 
     @Override
     public void run(String... args) throws Exception{
         Faker faker = new Faker();
-        Random random = new Random();
 
         for (int i = 0; i < 3; i++) {
             Categoria categoria = new Categoria();
+
             categoria.setIdCategoria(i);
             categoria.setNombreCategoria(faker.company().name());
             categoria.setCategorias(null);
+
+            categoriaRepository.save(categoria);
         }
 
         for (int i = 0; i < 3; i++) {
             Categorias categorias = new Categorias();
+
+            List<Categoria> categorias1 = new ArrayList<>();
+            List<Producto> productos1 = new ArrayList<>();
+
             categorias.setIdCategorias(i);
-            categorias.setCategoria(null);
-            categorias.setProducto(null);
+            categorias.setCategoria(categorias1);
+            categorias.setProducto(productos1);
+
+            categoriasRepository.save(categorias);
         }
 
         for (int i = 0; i < 3; i++) {
             Imagen imagen = new Imagen();
             Producto producto = new Producto();
+
             imagen.setIdImagen(i);
             imagen.setNombreImagen(faker.animal().name());
             imagen.setProducto(producto);
+
+            imagenRepository.save(imagen);
         }
 
         for (int i = 0; i < 3; i++) {
             Marca marca = new Marca();
+
             marca.setIdMarca(i);
             marca.setNombreMarca(faker.company().name());
             marca.setMarcas(null);
+
+            marcaRepository.save(marca);
         }
 
         for (int i = 0; i < 3; i++) {
             Marcas marcas = new Marcas();
+
+            List<Marca> marcas1 = new ArrayList<>();
+            List<Producto> productos1 = new ArrayList<>();
+
             marcas.setIdMarcas(i);
-            marcas.setMarcas(null);
-            marcas.setProductos(null);
+            marcas.setMarcas(marcas1);
+            marcas.setProductos(productos1);
+
+            marcasRepository.save(marcas);
         }
 
         for (int i = 0; i < 3; i++) {
             Producto producto = new Producto();
+            Marcas marcas = new Marcas();
+            Categorias categorias = new Categorias();
+
+            List<Imagen> imagenes = new ArrayList<>();
+
+            categorias.setIdCategorias(i);
+            categorias.setCategoria(null);
+            categorias.setProducto(null);
+
+            marcas.setIdMarcas(i);
+            marcas.setMarcas(null);
+            marcas.setProductos(null);
+
             producto.setIdProducto(i);
             producto.setNombreProducto(faker.book().title());
+            producto.setPrecio(342859.0);
+            producto.setDescripcionProducto(faker.famousLastWords().lastWords());
+            producto.setStock(631);
+            producto.setMarcas(marcas);
+            producto.setCategorias(categorias);
+            producto.setProductos(null);
+            producto.setImagenes(imagenes);
+
+            productoRepository.save(producto);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            Productos productos = new Productos();
+
+            List<Producto> productos1 = new ArrayList<>();
+            List<Integer> detallesBoletas = new ArrayList<>();
+
+            productos.setIdProductos(i);
+            productos.setProducto(productos1);
+            productos.setId_detalleBoletas(detallesBoletas);
+
+            productosRepository.save(productos);
         }
     } 
 
