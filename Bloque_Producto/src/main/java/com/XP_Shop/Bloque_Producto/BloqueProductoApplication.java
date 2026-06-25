856d9cf233ca81f5939bd5af5faf9f1b205bdff0
unknown <clau.osoriot@duocuc.cl>
2026-06-25 19:49:40 -0400

    avance conexion producto -> boleta y producto mockito

--- Bloque_Producto/src/main/java/com/XP_Shop/Bloque_Producto/BloqueProductoApplication.java
+++ Bloque_Producto/src/main/java/com/XP_Shop/Bloque_Producto/BloqueProductoApplication.java
@@ -2,8 +2,10 @@ package com.XP_Shop.Bloque_Producto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BloqueProductoApplication {
 
 	public static void main(String[] args) {
--- Bloque_Usuario/src/main/java/com/XP_Shop/Bloque_Usuario/BloqueUsuarioApplication.java
+++ Bloque_Usuario/src/main/java/com/XP_Shop/Bloque_Usuario/BloqueUsuarioApplication.java
@@ -2,8 +2,10 @@ package com.XP_Shop.Bloque_Usuario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BloqueUsuarioApplication {
 
 	public static void main(String[] args) {
