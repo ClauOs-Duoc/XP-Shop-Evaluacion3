e9acd4332e1f312db7a79a89ac1e58bead7b9c11
unknown <clau.osoriot@duocuc.cl>
2026-06-25 12:34:05 -0400

    gateway, eureka y jammer hechos

--- Bloque_Boleta/src/main/java/com/XP_Shop/Bloque_Boleta/BloqueBoletaApplication.java
+++ Bloque_Boleta/src/main/java/com/XP_Shop/Bloque_Boleta/BloqueBoletaApplication.java
@@ -2,8 +2,10 @@ package com.XP_Shop.Bloque_Boleta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BloqueBoletaApplication {
 
 	public static void main(String[] args) {
---
+++ eureka/src/test/java/com/example/xp_shop/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xp_shop.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
